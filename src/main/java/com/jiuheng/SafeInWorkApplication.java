2bef0c34f1958c55195f43c87c91c71a66cf55c7
cmq <3177783007@qq.com>
2022-09-18 17:24:09 +0800

    框架搭建及测试完成

--- src/main/java/com/jiuheng/SafeInWorkApplication.java
+++ src/main/java/com/jiuheng/SafeInWorkApplication.java
@@ -1,9 +1,11 @@
 package com.jiuheng;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.jiuheng.mapper")
 public class SafeInWorkApplication {
 
     public static void main(String[] args) {
